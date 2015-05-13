ebd5e6c473124718269605b5118550c1d4100e55
Gunnar Giffey <gunnar@textcontrol.com>
2015-05-13 11:49:09 +0200

    Added content.

---
+++ Form1.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using TXTextControl;
+
+namespace tx_reporting_customprocessing
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void mergeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            DataSet ds = new DataSet();
+            ds.ReadXml("sample_db.xml", XmlReadMode.Auto);
+
+            mailMerge1.Merge(ds.Tables["Person_Address"]);
+            
+        }
+
+        private void mailMerge1_DataRowMerged(object sender, TXTextControl.DocumentServer.MailMerge.DataRowMergedEventArgs e)
+        {
+            using (TXTextControl.ServerTextControl tx = new TXTextControl.ServerTextControl())
+            {
+                tx.Create();
+
+                tx.Load(e.MergedRow, TXTextControl.BinaryStreamType.InternalUnicodeFormat);
+
+                foreach (Table table in tx.Tables)
+                {
+                    table.AutoSize(tx);
+                }
+
+                byte[] data;
+
+                tx.Save(out data, BinaryStreamType.InternalUnicodeFormat);
+                e.MergedRow = data;
+            }
+        }
+
+
+    }
+}
---
+++ TableExtender.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TXTextControl;
+
+namespace TXTextControl
+{
+    public static class TableExtender
+    {
+        public static void AutoSize(this Table table, ServerTextControl TextControl)
+        {
+            int[] iColWidths = new int[table.Columns.Count];
+
+            foreach (TXTextControl.TableCell tc in table.Cells)
+            {
+                int textBounds = 0;
+
+                // check the width of every line in a cell
+                for (int i = tc.Start; i <= tc.Start + tc.Length - 1; i++)
+                {
+                    // pick width, if the current one is the longest
+                    if (TextControl.Lines.GetItem(i).TextBounds.Width > textBounds)
+                    {
+                        textBounds = TextControl.Lines.GetItem(i).TextBounds.Width;
+                    }
+                }
+
+                // pick the width, if it is the longest in the whole column
+                if (textBounds > (int)iColWidths.GetValue(tc.Column - 1))
+                {
+                    iColWidths.SetValue(textBounds, tc.Column - 1);
+                }
+            }
+
+            // resize the table due to the filled array
+            for (int iColCount = 1; iColCount <= table.Columns.Count; iColCount++)
+            {
+                if ((int)iColWidths.GetValue(iColCount - 1) == 0)
+                {
+                }
+                else
+                {
+                    table.Columns.GetItem(iColCount).Width = (int)iColWidths.GetValue(iColCount - 1) +
+                        table.Columns.GetItem(iColCount).CellFormat.RightTextDistance +
+                        table.Columns.GetItem(iColCount).CellFormat.LeftTextDistance +
+                        table.Columns.GetItem(iColCount).CellFormat.RightBorderWidth +
+                        table.Columns.GetItem(iColCount).CellFormat.LeftBorderWidth;
+                }
+            }
+        }
+    }
+}
